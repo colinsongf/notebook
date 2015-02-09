b186f9de8867fbf33e26735a0dda454aa76734aa
Rich Signell <rsignell@usgs.gov>
2015-02-09 17:58:30 -0500

    adding WCS

--- IOOS_Models.py
+++ IOOS_Models.py
@@ -7,29 +7,40 @@
 
 # <codecell>
 
-from IPython.display import HTML
-HTML('<iframe src=http://scitools.org.uk/iris/ width=800 height=350></iframe>')
-
-# <codecell>
-
-import matplotlib.pyplot as plt
-import numpy as np
 import datetime as dt
 import time
-import numpy.ma as ma
 
-import iris
-%matplotlib inline
+# <markdowncell>
+
+# Note: `iris` is not a default package in Wakari or Anaconda, [but installation is easy](https://github.com/ioos/conda-recipes/issues/11). 
 
 # <codecell>
 
-def time_near(cube,start):
-    #    coord_names = [coord.name() for coord in cube.coords()]
-    #    timevar = cube.coord(coord_names[0]))
-    timevar=cube.coord('time')
+import iris
+
+# <codecell>
+
+def find_timevar(cube):
+    """Return the variable attached to
+    time axis and rename it to time."""
     try:
-        itime = timevar.nearest_neighbour_index(timevar.units.date2num(start))
+        cube.coord(axis='T').rename('time')
+        print('Renaming {} to time'.format(cube.coord('time').var_name))
     except:
+        pass
+    timevar = cube.coord('time')
+    return timevar
+
+# <codecell>
+
+def time_near(cube, start):
+    """Return the nearest time to `start`.
+    TODO: Adapt to the new slice syntax"""
+    timevar = find_timevar(cube)
+    try:
+        time1 = timevar.units.date2num(start)
+        itime = timevar.nearest_neighbour_index(time1)
+    except IndexError:
         itime = -1
     return timevar.points[itime]
 
@@ -37,7 +48,8 @@ def time_near(cube,start):
 
 def var_lev_date(url=None,var=None,mytime=None,lev=0,subsample=1):
     time0= time.time()
-    cube = iris.load(url,iris.Constraint(name=var.strip()))[0]
+#    cube = iris.load_cube(url,iris.Constraint(name=var.strip()))[0]
+    cube = iris.load_cube(url,var)
 #    cube = iris.load(url,var)[0]
 #    print cube.coord('time')
 
@@ -52,25 +64,26 @@ def var_lev_date(url=None,var=None,mytime=None,lev=0,subsample=1):
 
 # <codecell>
 
-def myplot(myslice,model=None):
+def myplot(slice,model=None):
     # make the plot
-    plt.figure(figsize=(12,8))
-    lat=myslice.coord(axis='Y').points
-    lon=myslice.coord(axis='X').points
-    time=myslice.coord('time')[0]
-    plt.subplot(111,aspect=(1.0/np.cos(lat.mean()*np.pi/180.0)))
-    plt.pcolormesh(lon,lat,ma.masked_invalid(myslice.data));
-    plt.colorbar()
-    plt.grid()
+    figure(figsize=(12,8))
+    lat=slice.coord(axis='Y').points
+    lon=slice.coord(axis='X').points
+    time=slice.coord('time')[0]
+    subplot(111,aspect=(1.0/cos(mean(lat)*pi/180.0)))
+    pcolormesh(lon,lat,ma.masked_invalid(slice.data));
+    colorbar()
+    grid()
     date=time.units.num2date(time.points)
     date_str=date[0].strftime('%Y-%m-%d %H:%M:%S %Z')
-    plt.title('%s: %s: %s' % (model,myslice.long_name,date_str));
+    plt.title('%s: %s: %s' % (model,slice.long_name,date_str));
 
 # <codecell>
 
 # use contraints to select nearest time
 #mytime=dt.datetime(2008,7,28,12)  #specified time...
 mytime=dt.datetime.utcnow()      # .... or now
+print mytime
 
 # <codecell>
 
@@ -113,7 +126,7 @@ myplot(slice,model=model)
 model='HIOOS'
 url='http://oos.soest.hawaii.edu/thredds/dodsC/hioos/roms_assim/hiig/ROMS_Hawaii_Regional_Ocean_Model_Assimilation_best.ncd'
 var='sea_water_potential_temperature'
-lev=1
+lev=0
 slice=var_lev_date(url=url,var=var, mytime=mytime, lev=lev)
 myplot(slice,model=model)
 
@@ -122,7 +135,7 @@ myplot(slice,model=model)
 model='Global RTOFS/NCEP'
 url='http://ecowatch.ncddc.noaa.gov/thredds/dodsC/hycom/hycom_reg1_agg/HYCOM_Region_1_Aggregation_best.ncd'
 var='sea_water_temperature'  
-lev=0
+lev=1
 subsample=1
 slice=var_lev_date(url=url,var=var, mytime=mytime, lev=lev, subsample=subsample)
 myplot(slice,model=model)
@@ -133,22 +146,7 @@ print slice
 
 # <codecell>
 
-model='MARACOOS/ESPRESSO'
-url='http://tds.marine.rutgers.edu/thredds/dodsC/roms/espresso/2013_da/his_Best/ESPRESSO_Real-Time_v2_History_Best_Available_best.ncd'
-cubes=iris.load(url)
-
-# <codecell>
-
-var='potential temperature'
-cube = iris.load(url,iris.Constraint(name=var.strip()))
-
-# <codecell>
-
-cubes = iris.load(url)
-
-# <codecell>
-
-len(cube)
+!ls IOOS*
 
 # <codecell>
 
