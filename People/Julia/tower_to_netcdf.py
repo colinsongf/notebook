71a94348cba3d39c0a035ad4da7816f45b648242
Kyle Wilcox <kyle@axiomalaska.com>
2015-04-13 16:28:57 -0400

    Fix Tower to NetCDF scripts to use `pyaxiom`

--- People/Julia/tower_to_netcdf.py
+++ People/Julia/tower_to_netcdf.py
@@ -5,93 +5,95 @@
 
 # Save Tower CSV data as NetCDF
 
-# <codecell>
+# <markdowncell>
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import netCDF4
-import datetime as dt
-import urllib
-%matplotlib inline
+# ### Set local variables
 
 # <codecell>
 
 url='http://geoport.whoi.edu/thredds/fileServer/usgs/data2/notebook/data/CR3000_SN3557_Table1_MainTowerCR3000_ground_V6.CR3.txt'
-input_data="/usgs/data2/notebook/data/CR3000_SN3557_Table1_MainTowerCR3000_ground_V6.CR3.dat"
-output_dir="/usgs/data2/notebook/data"
+input_data="data.txt"
+output_dir="/data"
 output_file="julia.nc"
+fillvalue=-9999.9
+
+# <markdowncell>
+
+# ### Download the data
 
 # <codecell>
 
-urllib.urlretrieve (url,input_data)
+import urllib
+urllib.urlretrieve(url, input_data)
 
 # <codecell>
 
-df = pd.read_csv(input_data,skiprows=[0,2,3],parse_dates=True,index_col=0,low_memory=False)
-
-# <codecell>
-
+import pandas as pd
+df = pd.read_csv(input_data,skiprows=[0,2,3],
+                 parse_dates=True,
+                 index_col='TIMESTAMP',
+                 low_memory=False,
+                 na_values=['NAN',''],
+                 tupleize_cols=True)
+df = df.fillna(fillvalue)
 df.head()
 
+# <markdowncell>
+
+# ### Simple plot
+
 # <codecell>
 
+import matplotlib.pyplot as plt
+%matplotlib inline
 df[['Tsoil10cmTree_Avg','Tsoil20cmTree_Avg']].plot(figsize=(12,4));
 
 # <markdowncell>
 
-# Now we use some tools from pytools to write a single time series to a netCDF file.  Right now it is set up to only handle one variable, so it's the total solution for dataframes with more than one column of time series data.   Each variable could be written to a separate file and then merged together using NCO (or NcML). 
-# 
-# To use the timeseries create tool, we pass in a list of tuples data=[(time, vertical, value)] or three numpy arrays times=, verticals=, values=, where time is seconds since epoch, and the verticals are the heights.  The code was written for tide gauge data, and writes the vertical as positive down, and relative to sea surface.  So it should be modified for land applications. 
+# ### Create netCDF file
 
 # <codecell>
 
-#from pytools.netcdf.sensors import create,ncml,merge,crawl
-from pyaxiom.netcdf.sensors import create,ncml,merge,crawl
-
-# <codecell>
-
+import numpy as np
 def pd_to_secs(df):
-    """
-    convert a pandas datetime index to seconds since 1970
-    """
+    # convert a pandas datetime index to seconds since 1970
     import calendar
     return np.asarray([ calendar.timegm(x.timetuple()) for x in df.index ], dtype=np.int64)
 
-# <codecell>
-
-secs = pd_to_secs(df)
-
-# <codecell>
-
-# set sensor height to 10 m
-z = 10.0*np.ones_like(secs)
-
-# <codecell>
-
-# add units 
-attributes={'units':'Celcius'}
+def cf_safe_name(name):
+    # Create a CF safe name for a group/dimension/variable
+    import re
+    if isinstance(name, basestring):
+        if re.match('^[0-9_]', name):
+            # Add a letter to the front
+            name = "v_{}".format(name)
+        return re.sub(r'[^_a-zA-Z0-9]', "_", name)
+    return name
 
 # <codecell>
 
-values = df['Tsoil10cmTree_Avg'].values
+import os
+out_file = os.path.join(output_dir, output_file)
+if os.path.isfile(out_file):
+    os.remove(out_file)
+
+from pyaxiom.netcdf.sensors import TimeSeries
+ts = TimeSeries(output_dir,
+                latitude=0.39,
+                longitude=36.7,
+                station_name='urn:ioos:station:edu.princeton.ecohydrolab:MainTower',
+                global_attributes={},
+                times=pd_to_secs(df),
+                verticals=[10],
+                output_filename=output_file)
 
 # <codecell>
 
-sensor_urn='urn:edu.princeton.ecohydrolab:sensor:Tsoil10cmTree_Avg'
-station_urn='urn:edu.princeton.ecohydrolab:station:MainTower'
+for c in df.columns[::-1]:
+    # Add units based on column name?
+    var_attributes = dict()
+    ts.add_variable(cf_safe_name(c), df[c].values, attributes=var_attributes, fillvalue=-9999.9)
 
 # <codecell>
 
-create.create_timeseries_file(output_directory=output_dir,
-                              latitude=0.39,
-                              longitude=36.7,
-                              full_station_urn=station_urn,
-                              full_sensor_urn=sensor_urn,
-                              times=secs,
-                              verticals=z, 
-                              values=values,
-                              attributes=attributes,
-                              global_attributes={},
-                              output_filename=output_file)
 
