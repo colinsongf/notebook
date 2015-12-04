3febc7adc7e4acb25a1903706baa1cd5be2dc75c
rsignell-usgs <rsignell@usgs.gov>
2015-12-04 08:10:16 -0500

    updated fvcom_depth_and_vel for python3

--- UGRID/FVCOM_depth_and_velocity.py
+++ UGRID/FVCOM_depth_and_velocity.py
@@ -1,11 +1,7 @@
-# -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
 
-# <headingcell level=1>
+# coding: utf-8
 
-# Access data from the NECOFS (New England Coastal Ocean Forecast System) via OPeNDAP
-
-# <markdowncell>
+# # Access data from the NECOFS (New England Coastal Ocean Forecast System) via OPeNDAP
 
 # Demonstration using the NetCDF4-Python library to access velocity data from a triangular grid ocean model (FVCOM) via OPeNDAP, specifying the desired URL, time, layer and lat/lon region of interest.  The resulting plot of forecast velocity vectors over color-shaded bathymetry is useful for a variety of recreational and scientific purposes. 
 # 
@@ -16,16 +12,17 @@
 # * Model description: http://fvcom.smast.umassd.edu/research_projects/NECOFS/model_system.html
 # * THREDDS server with other forecast and archive products: http://www.smast.umassd.edu:8080/thredds/catalog.html
 
-# <codecell>
+# In[1]:
 
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.tri as Tri
 import netCDF4
 import datetime as dt
-%matplotlib inline
+get_ipython().magic('matplotlib inline')
 
-# <codecell>
+
+# In[2]:
 
 # DAP Data URL
 # MassBay GRID
@@ -33,23 +30,28 @@ url = 'http://www.smast.umassd.edu:8080/thredds/dodsC/FVCOM/NECOFS/Forecasts/NEC
 # GOM3 GRID
 #url='http://www.smast.umassd.edu:8080/thredds/dodsC/FVCOM/NECOFS/Forecasts/NECOFS_GOM3_FORECAST.nc'
 # Open DAP
-nc = netCDF4.Dataset(url).variables
-nc.keys()
+nc = netCDF4.Dataset(url)
+ncv = nc.variables
+ncv.keys()
 
-# <codecell>
+
+# In[4]:
 
 # take a look at the "metadata" for the variable "u"
-print nc['u']
+print(ncv['u'])
 
-# <codecell>
 
-nc['temp'].shape
+# In[5]:
 
-# <codecell>
+ncv['temp'].shape
 
-nc['nv'].shape
 
-# <codecell>
+# In[6]:
+
+ncv['nv'].shape
+
+
+# In[7]:
 
 # Desired time for snapshot
 # ....right now (or some number of hours from now) ...
@@ -57,30 +59,33 @@ start = dt.datetime.utcnow() + dt.timedelta(hours=+4)
 # ... or specific time (UTC)
 #start = dt.datetime(2013,3,2,15,0,0)
 
-# <codecell>
+
+# In[9]:
 
 # Get desired time step  
-time_var = nc['time']
+time_var = ncv['time']
 itime = netCDF4.date2index(start,time_var,select='nearest')
 
 # Get lon,lat coordinates for nodes (depth)
-lat = nc['lat'][:]
-lon = nc['lon'][:]
+lat = ncv['lat'][:]
+lon = ncv['lon'][:]
 # Get lon,lat coordinates for cell centers (depth)
-latc = nc['latc'][:]
-lonc = nc['lonc'][:]
+latc = ncv['latc'][:]
+lonc = ncv['lonc'][:]
 # Get Connectivity array
-nv = nc['nv'][:].T - 1 
+nv = ncv['nv'][:].T - 1 
 # Get depth
-h = nc['h'][:]  # depth 
+h = ncv['h'][:]  # depth 
 
-# <codecell>
+
+# In[10]:
 
 tm = netCDF4.num2date(time_var[itime],time_var.units)
 daystr = tm.strftime('%Y-%b-%d %H:%M')
-print daystr
+print(daystr)
 
-# <codecell>
+
+# In[12]:
 
 # round to nearest 10 minutes to make titles look better
 tm += dt.timedelta(minutes=5)
@@ -88,20 +93,23 @@ tm -= dt.timedelta(minutes=tm.minute % 10,
                          seconds=tm.second,
                          microseconds=tm.microsecond)
 daystr = tm.strftime('%Y-%b-%d %H:%M')
-print daystr
+print(daystr)
 
-# <codecell>
+
+# In[13]:
 
 tri = Tri.Triangulation(lon,lat, triangles=nv)
 
-# <codecell>
+
+# In[14]:
 
 # get current at layer [0 = surface, -1 = bottom]
 ilayer = 0
-u = nc['u'][itime, ilayer, :]
-v = nc['v'][itime, ilayer, :]
+u = ncv['u'][itime, ilayer, :]
+v = ncv['v'][itime, ilayer, :]
 
-# <codecell>
+
+# In[15]:
 
 #boston harbor
 levels=np.arange(-34,2,1)   # depth contours to plot
@@ -109,7 +117,8 @@ ax= [-70.97, -70.82, 42.25, 42.35] #
 maxvel = 0.5
 subsample = 3
 
-# <codecell>
+
+# In[16]:
 
 #woods hole
 levels=np.arange(-30,2,1)
@@ -118,18 +127,21 @@ ax = [-70.75, -70.6, 41.48, 41.56]
 maxvel = 1.0
 subsample = 2
 
-# <codecell>
+
+# In[17]:
 
 # find velocity points in bounding box
 ind = np.argwhere((lonc >= ax[0]) & (lonc <= ax[1]) & (latc >= ax[2]) & (latc <= ax[3]))
 
-# <codecell>
+
+# In[18]:
 
 np.random.shuffle(ind)
 Nvec = int(len(ind) / subsample)
 idv = ind[:Nvec]
 
-# <codecell>
+
+# In[19]:
 
 # tricontourf plot of water depth with vectors on top
 fig=plt.figure(figsize=(18,10))
@@ -143,4 +155,19 @@ Q = plt.quiver(lonc[idv],latc[idv],u[idv],v[idv],scale=20)
 maxstr='%3.1f m/s' % maxvel
 qk = plt.quiverkey(Q,0.92,0.08,maxvel,maxstr,labelpos='W')
 plt.title('NECOFS Velocity, Layer %d, %s UTC' % (ilayer, daystr));
+
+
+# In[20]:
+
+nc.filepath()
+
+
+# In[21]:
+
+nc.get_variables_by_attributes(standard_name='sea_water_salinity')
+
+
+# In[ ]:
+
+
 
